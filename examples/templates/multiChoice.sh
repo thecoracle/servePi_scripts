7a620df442f97addf6d8bda528c3d099271cc8ee
theCoracle <eat@servepi.com>
2018-11-11 04:40:51 -0700

    first update

---
+++ examples/templates/multiChoice.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+##################################################
+# Script: multiChoice.sh
+# Author: TheCoracle
+# Creation Date: 2018-04-19
+# Edit: 
+#
+# Purpose: Run script to select each type of pi project
+# so you don't have to manually install everything
+#
+# Notes:
+##################################################
+
+### PREP #########################################
+export PATH="$PATH:/home/naspi/scripts/setup/"
+
+# COLORS #
+## No Colors ##
+NC='\033[0m'
+
+## Regular Colors ##
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+### PREP END #####################################
+
+### SCRIPT #######################################
+
+### CHOOSE PI ####################################
+choice () {
+  local choice=$1
+  if [[ ${opts[choice]} ]] # toggle
+  then
+    opts[choice]=
+  else
+    opts[choice]=◉
+  fi
+}
+
+PS3='Please enter your choice: '
+while :
+  do
+    clear
+    options=("Option 1 ${opts[1]}" "Option 2 ${opts[2]}" "Option 3 ${opts[3]}" "Option 4 ${opts[4]}" "Option 5 ${opts[5]}" "Option 6 ${opts[6]}" "Option 7 ${opts[7]}" "Option 8 ${opts[8]}"  "Done")
+    select opt in "${options[@]}"
+      do
+        case $opt in
+          "Option 1 ${opts[1]}")
+            choice 1
+            break
+            ;;
+          "Option 2 ${opts[2]}")
+            choice 2
+            break
+            ;;
+          "Option 3 ${opts[3]}")
+            choice 3
+            break
+            ;;
+          "Option 4 ${opts[4]}")
+            choice 4
+            break
+            ;;
+          "Option 5 ${opts[5]}")
+            choice 5
+            break
+            ;;
+          "Option 6 ${opts[6]}")
+            choice 6
+            break
+            ;;
+          "Option 7 ${opts[7]}")
+            choice 7
+            break
+            ;;
+          "Option 8 ${opts[8]}")
+            choice 8
+            break
+            ;;
+          "Done")
+            break 2
+            ;;
+          *) printf '%s\n' 'invalid option';;
+        esac
+    done
+done
+
+printf '%s\n' 'Options chosen:'
+for opt in "${!opts[@]}"
+do
+    if [[ ${opts[opt]} ]]
+    then
+        printf '%s\n' "Option $opt"
+    fi
+done
---
+++ examples/templates/setupPi.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+# ---------------------------------------------------------------------------
+# setupPi.sh - Adjust settings and install software for new Pi after initial install
+
+# Copyright 2018, nasPi,,, <naspi@nasPi>
+# All rights reserved.
+
+# Usage: setupPi.sh [-h|--help]
+
+# Revision history:
+# 2018-04-19 Created by newScript.sh ver. 3.3
+# ---------------------------------------------------------------------------
+
+PROGNAME=${0##*/}
+VERSION="0.1"
+
+clean_up() { # Perform pre-exit housekeeping
+  return
+}
+
+error_exit() {
+  echo -e "${PROGNAME}: ${1:-"Unknown Error"}" >&2
+  clean_up
+  exit 1
+}
+
+graceful_exit() {
+  clean_up
+  exit
+}
+
+signal_exit() { # Handle trapped signals
+  case $1 in
+    INT)
+      error_exit "Program interrupted by user" ;;
+    TERM)
+      echo -e "\n$PROGNAME: Program terminated" >&2
+      graceful_exit ;;
+    *)
+      error_exit "$PROGNAME: Terminating on unknown signal" ;;
+  esac
+}
+
+usage() {
+  echo -e "Usage: $PROGNAME [-h|--help]"
+}
+
+help_message() {
+  cat <<- _EOF_
+  $PROGNAME ver. $VERSION
+  Adjust settings and install software for new Pi after initial install
+
+  $(usage)
+
+  Options:
+  -h, --help  Display this help message and exit.
+
+  NOTE: You must be the superuser to run this script.
+
+_EOF_
+  return
+}
+
+# Trap signals
+trap "signal_exit TERM" TERM HUP
+trap "signal_exit INT"  INT
+
+# Check for root UID
+if [[ $(id -u) != 0 ]]; then
+  error_exit "You must be the superuser to run this script."
+fi
+
+# Parse command-line
+while [[ -n $1 ]]; do
+  case $1 in
+    -h | --help)
+      help_message; graceful_exit ;;
+    -* | --*)
+      usage
+      error_exit "Unknown option $1" ;;
+    *)
+      echo "Argument $1 to process..." ;;
+  esac
+  shift
+done
+
+export PATH="$PATH:/home/TheMacPC/scripts/"
+
+#COLORS
+# No Colors
+NC='\033[0m'
+
+# Regular Colors
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+
+set -eu -o pipefail # fail on error , debug all lines
+
+# Main logic
+
+choice () {
+    local choice=$1
+    if [[ ${opts[choice]} ]] # toggle
+    then
+        opts[choice]=
+    else
+        opts[choice]=◉
+    fi
+}
+
+PS3='Please enter your choice: '
+while :
+do
+    clear
+    options=("Option 1 ${opts[1]}" "Option 2 ${opts[2]}" "Option 3 ${opts[3]}" "Done")
+    select opt in "${options[@]}"
+    do
+        case $opt in
+            "Option 1 ${opts[1]}")
+                choice 1
+                break
+                ;;
+            "Option 2 ${opts[2]}")
+                choice 2
+                break
+                ;;
+            "Option 3 ${opts[3]}")
+                choice 3
+                break
+                ;;
+            "Option 4 ${opts[4]}")
+                choice 4
+                break
+                ;;
+            "Done")
+                break 2
+                ;;
+            *) printf '%s\n' 'invalid option';;
+        esac
+    done
+done
+
+printf '%s\n' 'Options chosen:'
+for opt in "${!opts[@]}"
+do
+    if [[ ${opts[opt]} ]]
+    then
+        printf '%s\n' "Option $opt"
+    fi
+done
+
+
+graceful_exit
+
